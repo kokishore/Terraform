29626d367f22124a747aee8b1f490fb6edcdfdcd
Kothapalli Kishore (LTIMINDTREE LIMITED) <v-kokishore@microsoft.com>
2025-06-16 03:40:15 -0700

    exec

---
+++ provisioners/remote-exec/variables.tf
@@ -0,0 +1,44 @@
+variable "ami_id" {
+  type        = string
+  default     = "ami-09c813fb71547fc4f"
+  description = "AMI ID of the RHEL9 DevOps Practice"
+}
+
+variable "instance_type" {
+    default = "t3.micro"
+}
+
+variable "ec2_tags" {
+    type   = map(string)
+    default = {
+        Name = "Kishore_Instance"
+        Purpose = "practise"
+    }
+}
+
+variable "sg_name" {
+    default = "allow_all"
+    
+}
+
+variable "sg_description" {
+    default = "allowing all traffic"
+}
+
+variable "from_port" {
+    default = 0
+}
+
+variable "to_port" {
+    default = 0
+}
+
+variable "cidr_blocks" {
+    default = ["0.0.0.0/0"]
+}
+
+variable "sg_tags" {
+    default = {
+        Name = "allow-all"
+    }
+}
