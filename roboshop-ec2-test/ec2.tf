a4514213e889ebcfa09a5d758fbba3ff64f4c02f
Kothapalli Kishore (LTIMINDTREE LIMITED) <v-kokishore@microsoft.com>
2025-07-04 02:21:18 -0700

    terraform

---
+++ roboshop-ec2-test/ec2.tf
@@ -0,0 +1,6 @@
+module "ec2" {
+    source = "../terraform-module"
+    sg_ids = var.security_group_ids
+    instance_type = var.instance_type
+    tags = var.tags
+}
---
+++ roboshop-ec2-test/variables.tf
@@ -0,0 +1,15 @@
+variable "security_group_ids" {
+    default = ["sg-03a1954722cbb5a31"]
+}
+
+variable "tags" {
+    default = {
+        Name = "roboshop-cart"
+        Terraform = "true"
+        Environment = "dev"
+    }
+}
+
+variable "instance_type" {
+    default = "t3.small"
+}
